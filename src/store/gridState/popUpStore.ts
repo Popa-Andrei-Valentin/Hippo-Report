8d40736100c0a8bd5e021ac33d2842a0bd455c63
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-05-29 22:54:41 +0300

    Created popup store

---
+++ src/store/gridState/popUpStore.ts
@@ -0,0 +1,49 @@
+export default {
+    state: {
+        isPopUpOpen: false,
+        popUpTitle: "",
+        popUpText: ""
+    },
+    getters: {
+        isPopUpOpenStatus(state: {isPopUpOpen: boolean}): boolean {
+            return state.isPopUpOpen;
+        },
+
+        getPopUpTitle(state: {popUpTitle: string}): string {
+            return state.popUpTitle;
+        },
+
+        getPopUpText(state: {popUpText: string}): string {
+            return state.popUpText;
+        }
+    },
+    mutations: {
+        setPopUpStatus(state: {isPopUpOpen: boolean}, status: boolean): void {
+            state.isPopUpOpen = status;
+        },
+
+        setPopUpTitle(state: {popUpTitle: string}, titleText: string): void {
+            state.popUpTitle = titleText;
+        },
+
+        setPopUpText(state: {popUpText: string}, popUpText: string): void {
+            state.popUpText = popUpText;
+        },
+    },
+    actions: {
+        //@ts-ignore
+       updatePopUpStatus({commit}, status: boolean): void {
+           commit('setPopUpStatus', status)
+       },
+
+        //@ts-ignore
+        updatePopUpTitle({commit}, title: string): void {
+            commit('setPopUpTitle', title)
+        },
+
+        //@ts-ignore
+        updatePopUpText({commit}, text: string): void {
+            commit('setPopUpText', text)
+        }
+    },
+}
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { createStore } from 'vuex'
 import gridStore from "@/store/gridState/gridStore";
+import popUpStore from "@/store/gridState/popUpStore";
 
 export default createStore({
   state: {
@@ -14,6 +15,7 @@ export default createStore({
     //@ts-ignore
     // TODO: FIX ACTION<any, {}> error
     // https://github.com/championswimmer/vuex-persist/issues/132
-    gridStore: gridStore
+    gridStore: gridStore,
+    popUpStore: popUpStore
   }
 })
